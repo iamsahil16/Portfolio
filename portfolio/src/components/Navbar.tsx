1b225bafb452c3730518800115eb4edccc913903
iamsahil16 <sahil0194@gmail.com>
2025-01-05 04:24:32 +0530

    navbar

---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../assets/kevinRushLogo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+
+
+function Navbar() {
+  return (
+    <>
+        <nav className=" mb-20 flex item center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-10" src={logo} alt="logo" />
+            </div>
+            <div className='m-8 flex items-center justify-center gap-4 text-2xl ' >
+                <a href='https://www.linkedin.com/in/sahil-singh-b8746529b/' target='_blank' rel="noopener noreferrer" 
+                aria-label='linkedin'>
+                    <FaLinkedin />
+                </a>
+                <a href='https://github.com/iamsahil16' target='_blank' rel="noopener noreferrer" 
+                aria-label='linkedin'>
+                    <FaGithub />
+                </a>
+                <a href='https://x.com/sahilsi67278574' target='_blank' rel="noopener noreferrer" 
+                    aria-label='linkedin'>
+                    <FaSquareXTwitter />
+                </a>
+                <a href='https://www.instagram.com/sahil_singh_8851/' target='_blank' rel="noopener noreferrer" 
+                    aria-label='linkedin'>
+                    <FaInstagram />
+                </a>
+
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
